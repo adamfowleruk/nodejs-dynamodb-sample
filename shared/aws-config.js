ca9e4ff6ad63eebf262b89c536a1f90c5c074b1f
adamfowleruk <adamfowleruk@gmail.com>
2017-08-13 14:04:26 +0100

    fix url

--- shared/aws-config.js
+++ shared/aws-config.js
@@ -2,14 +2,14 @@ var AWS = require("aws-sdk");
 
 AWS.config.update({
   region: "eu-west-1",
-  endpoint: "https://dynamodb.eu-west-1.amazonaws.com" // http://localhost:8000 or https://dynamodb.eu-west-1.amazonaws.com
+  endpoint: "http://localhost:8000" // http://localhost:8000 or https://dynamodb.eu-west-1.amazonaws.com
 });
 
 
 var awsconfig = {
   conf: {
     region: "eu-west-1",
-    endpoint: "https://dynamodb.eu-west-1.amazonaws.com" // http://localhost:8000 or https://dynamodb.eu-west-1.amazonaws.com
+    endpoint: "http://localhost:8000" // http://localhost:8000 or https://dynamodb.eu-west-1.amazonaws.com
   },
   docClient: new AWS.DynamoDB.DocumentClient(),
   table: "Movies"
